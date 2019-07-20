d1fbfde791fc96ae9b085ea04e30853aa0073e43
Subhajit Chatterjee <subhajitchatterjee425@gmail.com>
2019-07-20 09:39:18 +0530

    solved the animation problem and creation of continious child by removeChild

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
- window.addEventListener("load", ()=>{
+ window.addEventListener("load", () => {
+     "use strict";
      const sounds = document.querySelectorAll(".sound");//return an array
      const pads = document.querySelectorAll(".pads div");
-     const visual = document.querySelector("visual");
-     const color = [
+     const visual = document.querySelector(".visual");
+     const colors = [
         "#60d394",
         "#d36060",
         "#c060d3",
@@ -12,20 +13,24 @@
      ];
 
      //now for the sounds
-     pads.forEach((pad, index) =>{
-         pad.addEventListener('click', function(){
+     pads.forEach((pad, index) => {
+         pad.addEventListener("click", function(){
             sounds[index].currentTime= 0;
             sounds[index].play();
 
             createBubbles(index);
          });
      });
+
      //function for make bubbles
-     const createBubbles = (index) => {
+     const createBubbles = index => {
          const bubble = document.createElement("div");
          visual.appendChild(bubble);
-         bubble.style.backgroundColor = color[index];
+         bubble.style.backgroundColor = colors [index];
          bubble.style.animation = "jump 1s ease";
+         bubble.addEventListener("animationend", function(){
+             visual.removeChild(this);
+         });
      };
 
  });
